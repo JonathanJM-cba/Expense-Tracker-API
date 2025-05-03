557f827199903eeaa06c332fda4783a1f5d9834b
jonathan <jonathan20cba@gmail.com>
2025-05-02 22:04:58 -0300

    Se agrega validaciones al registro de usuario

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { createUser } = require("../controllers/userController");
+const { registerUserValidator } = require("../validations/userValidator");
 const router = express.Router();
 
-router.post("/register", createUser);
+router.post("/register", registerUserValidator, createUser);
 
 module.exports = router;
